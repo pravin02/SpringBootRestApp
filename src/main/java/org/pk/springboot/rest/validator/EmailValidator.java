74eaa75644565ded5d4cf374e05d8512eb0c31d1
Pravin P Patil <prvnpatil11@gmail.com>
2021-01-30 12:55:44 +0530

    EmailValidator sonar issues fixed

--- src/main/java/org/pk/springboot/rest/validator/EmailValidator.java
+++ src/main/java/org/pk/springboot/rest/validator/EmailValidator.java
@@ -5,18 +5,20 @@ import java.util.regex.Pattern;
 
 public class EmailValidator {
 
-	public static final String regExpn = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-	public static final Object pattern = Pattern.compile(regExpn, Pattern.CASE_INSENSITIVE);
+    public static final String REGEX = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+    public static final Object PATTERN = Pattern.compile(REGEX, Pattern.CASE_INSENSITIVE);
 
-	/**
-	 * This method is used to validate input emailId
-	 * 
-	 * @param inputEmail
-	 *            the emailId to validate
-	 * @return true if emailId is valid (in proper format), false otherwise
-	 */
-	public static boolean isEmailValid(String emailId) {
-		Object matcher = ((Pattern) pattern).matcher(emailId);
-		return emailId == null ? false : ((Matcher) matcher).matches();
-	}
+    private EmailValidator() {
+    }
+
+    /**
+     * This method is used to validate input emailId
+     *
+     * @param emailId the emailId to validate
+     * @return true if emailId is valid (in proper format), false otherwise
+     */
+    public static boolean isEmailValid(String emailId) {
+        Object matcher = ((Pattern) PATTERN).matcher(emailId);
+        return emailId == null ? Boolean.FALSE : ((Matcher) matcher).matches();
+    }
 }
