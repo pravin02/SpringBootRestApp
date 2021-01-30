cfd8714234c5b89dbfb45cf340d8cafa8fb44224
Pravin P Patil <prvnpatil11@gmail.com>
2021-01-30 12:31:25 +0530

    User class converted to Dto and returned as response

--- src/main/java/org/pk/springboot/rest/controller/AuthenticationController.java
+++ src/main/java/org/pk/springboot/rest/controller/AuthenticationController.java
@@ -1,6 +1,7 @@
 package org.pk.springboot.rest.controller;
 
 import org.pk.springboot.rest.domian.User;
+import org.pk.springboot.rest.dto.UserDto;
 import org.pk.springboot.rest.exception.LoginFailedException;
 import org.pk.springboot.rest.other.Response;
 import org.pk.springboot.rest.service.UserService;
@@ -17,28 +18,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/authentication")
 public class AuthenticationController {
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	/**
-	 * @param User
-	 *            object
-	 * @return this API return response status if user details are valid then it
-	 *         will return status true and data field will initialized with
-	 *         user details.
-	 */
-	@RequestMapping(value = "", method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE,
-			MediaType.APPLICATION_XML_VALUE })
-	public ResponseEntity<Response> add(@RequestBody User object) {
-		Response response = null;
-		HttpStatus httpStatus = HttpStatus.OK;
-		try {
-			object = userService.findByEmailAndPassword(object.getEmail(), object.getPassword());
-			response = new Response(true, object, "Logged in succesfully.");
-		} catch (LoginFailedException lfe) {
-			httpStatus = HttpStatus.NOT_FOUND;
-			response = new Response(false, null, lfe.getMessage());
-		}
-		return new ResponseEntity<>(response, httpStatus);
-	}
+    /**
+     * @param object
+     * @return this API return response status if user details are valid then it
+     * will return status true and data field will initialized with
+     * user details.
+     */
+
+    @RequestMapping(value = "", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<Response> add(@RequestBody UserDto object) {
+        Response response = null;
+        HttpStatus httpStatus = HttpStatus.OK;
+        try {
+            object = userService.findByEmailAndPassword(object.getEmail(), object.getPassword());
+            response = new Response(true, object, "Logged in succesfully.");
+        } catch (LoginFailedException lfe) {
+            httpStatus = HttpStatus.NOT_FOUND;
+            response = new Response(false, null, lfe.getMessage());
+        }
+        return new ResponseEntity<>(response, httpStatus);
+    }
 }
