32d6bdf334e25a403fde1b2ec4d7fe540c73eacf
Pravin P Patil <prvnpatil11@gmail.com>
2021-01-30 13:41:00 +0530

    sonar issues fixed for util package

--- src/main/java/org/pk/springboot/rest/util/DateTimeUtil.java
+++ src/main/java/org/pk/springboot/rest/util/DateTimeUtil.java
@@ -5,57 +5,59 @@ import java.util.Date;
 import java.util.Locale;
 
 /**
- * @author Pravin P Patil Utility class for dealing with date-time conversions
- *         in different formats
+ * @author Pravin P Patil
+ * @apiNote Utility class for dealing with date-time conversions
+ * in different formats
  */
 public class DateTimeUtil {
-	public static final String DATE_TIME_FORMAT = "dd/MM/yyyy HH:mm:ss";
-	public static final String FILE_NAME_FORMAT = "dd_MM_yyyy_HH_mm_ss";
-	public static final String DATE_FORMAT = "dd_MM_yyyy";
+    public static final String DATE_TIME_FORMAT = "dd/MM/yyyy HH:mm:ss";
+    public static final String FILE_NAME_FORMAT = "dd_MM_yyyy_HH_mm_ss";
 
-	/**
-	 * Method to get the current date-time in the input format.
-	 * 
-	 * @param dateTimeFormat
-	 *            the date-time format
-	 * @param timestamp
-	 *            the timestamp to be formatted
-	 * @return the current date-time in the input format
-	 */
-	public static String getFormattedTime(String dateTimeFormat, long timestamp) {
-		Date date = new Date(timestamp);
-		SimpleDateFormat sdf = new SimpleDateFormat(dateTimeFormat, Locale.US);
-		return sdf.format(date);
-	}
 
-	/**
-	 * Method to get the current date-time in the input format.
-	 * 
-	 * @param dateTimeFormat
-	 *            the date-time format
-	 * @return the current date-time in the input format
-	 */
-	public static String getFormattedTime(String dateTimeFormat) {
-		SimpleDateFormat sdf = new SimpleDateFormat(dateTimeFormat, Locale.US);
-		Date date = new Date(System.currentTimeMillis());
-		return sdf.format(date);
-	}
+    private DateTimeUtil() {
 
-	/**
-	 * @return current date time
-	 */
-	public static String getCurrentDateTime() {
-		SimpleDateFormat sdf = new SimpleDateFormat(DATE_TIME_FORMAT);
-		return sdf.format(new Date());
-	}
+    }
 
-	/**
-	 * @return current date time for file name
-	 */
-	public static String getCurrentDateTimeForFile() {
-		SimpleDateFormat sdf = new SimpleDateFormat(FILE_NAME_FORMAT);
-		return sdf.format(new Date());
-	}
+    /**
+     * Method to get the current date-time in the input format.
+     *
+     * @param dateTimeFormat the date-time format
+     * @param timestamp      the timestamp to be formatted
+     * @return the current date-time in the input format
+     */
+    public static String getFormattedTime(String dateTimeFormat, long timestamp) {
+        Date date = new Date(timestamp);
+        SimpleDateFormat sdf = new SimpleDateFormat(dateTimeFormat, Locale.US);
+        return sdf.format(date);
+    }
 
-	
+    /**
+     * Method to get the current date-time in the input format.
+     *
+     * @param dateTimeFormat the date-time format
+     * @return the current date-time in the input format
+     */
+    public static String getFormattedTime(String dateTimeFormat) {
+        SimpleDateFormat sdf = new SimpleDateFormat(dateTimeFormat, Locale.US);
+        Date date = new Date(System.currentTimeMillis());
+        return sdf.format(date);
+    }
+
+    /**
+     * @return current date time
+     */
+    public static String getCurrentDateTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_TIME_FORMAT);
+        return sdf.format(new Date());
+    }
+
+    /**
+     * @return current date time for file name
+     */
+    public static String getCurrentDateTimeForFile() {
+        SimpleDateFormat sdf = new SimpleDateFormat(FILE_NAME_FORMAT);
+        return sdf.format(new Date());
+    }
+
+
 }
--- src/main/java/org/pk/springboot/rest/util/StringUtil.java
+++ src/main/java/org/pk/springboot/rest/util/StringUtil.java
@@ -1,32 +1,34 @@
 package org.pk.springboot.rest.util;
 
 /**
- * Utility class for validations of string
- * 
+ * @author Pravin P Patil
+ * @apiNote Utility class for validations of string
  */
 public class StringUtil {
 
-	/**
-	 * @param str
-	 * @return
-	 */
-	public static boolean isEmpty(String str) {
-		if (str != null && !str.equals(""))
-			return false;
-		return true;
-	}
+    private StringUtil() {
 
-	/**
-	 * method to check passed string is digit or not
-	 * 
-	 * @param str
-	 * @return
-	 */
-	public static boolean isDigit(String str) {
-		char[] digits = str.toCharArray();
-		for (char c : digits)
-			if (!Character.isDigit(c))
-				return false;
-		return true;
-	}
+    }
+
+    /**
+     * @param str
+     * @return
+     */
+    public static boolean isEmpty(String str) {
+        return str != null && !str.equals("") ? Boolean.FALSE : Boolean.TRUE;
+    }
+
+    /**
+     * method to check passed string is digit or not
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isDigit(String str) {
+        char[] digits = str.toCharArray();
+        for (char c : digits)
+            if (!Character.isDigit(c))
+                return false;
+        return true;
+    }
 }
