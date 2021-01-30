d7c13a620e97bc907efca65c4bbe7277aa5f79e2
Pravin P Patil <prvnpatil11@gmail.com>
2021-01-30 13:38:11 +0530

    sonar issues fixed for exception package

--- src/main/java/org/pk/springboot/rest/exception/RestExceptionHandler.java
+++ src/main/java/org/pk/springboot/rest/exception/RestExceptionHandler.java
@@ -1,5 +1,7 @@
 package org.pk.springboot.rest.exception;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.pk.springboot.rest.other.Response;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -20,64 +22,62 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@Override
-	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
-			org.springframework.http.HttpHeaders headers, HttpStatus status, WebRequest request) {
-		System.out.println("handleHttpMessageNotReadable");
-		Response response = new Response(false, HttpStatus.BAD_REQUEST.name(), ex.getMessage());
-		return new ResponseEntity<Object>(response, HttpStatus.BAD_REQUEST);
-	}
+    private static final Logger APP_LOGGER = LogManager.getLogger(RestExceptionHandler.class);
 
-	@Override
-	protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		System.out.println("handleHttpMediaTypeNotAcceptable");
-		Response response = new Response(false, HttpStatus.NOT_ACCEPTABLE.name(), ex.getMessage());
-		return new ResponseEntity<Object>(response, HttpStatus.NOT_ACCEPTABLE);
-	}
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  org.springframework.http.HttpHeaders headers, HttpStatus status, WebRequest request) {
+        APP_LOGGER.error("handleHttpMessageNotReadable");
+        Response response = new Response(false, HttpStatus.BAD_REQUEST.name(), ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 
-	@Override
-	protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		System.out.println("handleHttpMediaTypeNotSupported");
-		Response response = new Response(false, HttpStatus.UNSUPPORTED_MEDIA_TYPE.name(), ex.getMessage());
-		return new ResponseEntity<Object>(response, HttpStatus.UNSUPPORTED_MEDIA_TYPE);
-	}
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex,
+                                                                      HttpHeaders headers, HttpStatus status, WebRequest request) {
+        APP_LOGGER.error("handleHttpMediaTypeNotAcceptable");
+        Response response = new Response(false, HttpStatus.NOT_ACCEPTABLE.name(), ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_ACCEPTABLE);
+    }
 
-	@Override
-	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		System.out.println("handleHttpRequestMethodNotSupported");
-		Response response = new Response(false, HttpStatus.METHOD_NOT_ALLOWED.name(), ex.getMessage());
-		return new ResponseEntity<Object>(response, HttpStatus.METHOD_NOT_ALLOWED);
-	}
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex,
+                                                                     HttpHeaders headers, HttpStatus status, WebRequest request) {
+        APP_LOGGER.error("handleHttpMediaTypeNotSupported");
+        Response response = new Response(false, HttpStatus.UNSUPPORTED_MEDIA_TYPE.name(), ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+    }
 
-	@Override
-	protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers,
-			HttpStatus status, WebRequest request) {
-		System.out.println("handleMissingPathVariable");
-		Response response = new Response(false, HttpStatus.METHOD_NOT_ALLOWED.name(), ex.getMessage());
-		return new ResponseEntity<Object>(response, HttpStatus.METHOD_NOT_ALLOWED);
-	}
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+                                                                         HttpHeaders headers, HttpStatus status, WebRequest request) {
+        APP_LOGGER.error("handleHttpRequestMethodNotSupported");
+        Response response = new Response(false, HttpStatus.METHOD_NOT_ALLOWED.name(), ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
+    }
 
-	@Override
-	protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers,
-			HttpStatus status, WebRequest request) {
-		System.out.println("handleNoHandlerFoundException");
-		Response response = new Response(false, HttpStatus.INTERNAL_SERVER_ERROR.name(), ex.getMessage());
-		return new ResponseEntity<Object>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+    @Override
+    protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers,
+                                                               HttpStatus status, WebRequest request) {
+        APP_LOGGER.error("handleMissingPathVariable");
+        Response response = new Response(false, HttpStatus.METHOD_NOT_ALLOWED.name(), ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
+    }
 
-	@Override
-	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
-			HttpStatus status, WebRequest request) {
-		System.out.println("handleExceptionInternal");
-		Response response = new Response(false, HttpStatus.INTERNAL_SERVER_ERROR.name(), ex.getMessage());
-		return new ResponseEntity<Object>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers,
+                                                                   HttpStatus status, WebRequest request) {
+        APP_LOGGER.error("handleNoHandlerFoundException");
+        Response response = new Response(false, HttpStatus.INTERNAL_SERVER_ERROR.name(), ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-	/*
-	 * private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
-	 * return new ResponseEntity<>(apiError, apiError.getStatus()); }
-	 */
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+                                                             HttpStatus status, WebRequest request) {
+        APP_LOGGER.error("handleExceptionInternal");
+        Response response = new Response(false, HttpStatus.INTERNAL_SERVER_ERROR.name(), ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
